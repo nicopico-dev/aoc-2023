98c73e65434f6f7e44391c0a00edb367bf38318f
Nicolas Picon <nicopico.dev@gmail.com>
2023-12-03 14:35:05 +0100

    Day 3 - Part 1: Get character at coordinates

--- src/main/kotlin/days/Day3.kt
+++ src/main/kotlin/days/Day3.kt
@@ -8,9 +8,23 @@ class Day3(
     }
 
     class Schematic(
-        description: String
+        private val data: String
     ) {
-        val width: Int = description.indexOf("\n")
-        val height: Int = description.count { it == '\n' }
+        val width: Int = data.indexOf("\n")
+        val height: Int = data.count { it == '\n' }
+
+        fun getCharacterAt(x: Int, y: Int): Char {
+            require(x in 0..<width) {
+                "x should be in range [0, $width[ ($x)"
+            }
+            require(y in 0..<height) {
+                "y should be in range [0, $height[ ($y)"
+            }
+
+            // Add 1 to width to count the `\n` character
+            val index = (y * (width + 1)) + x
+
+            return data[index]
+        }
     }
 }
--- src/test/kotlin/days/Day3Test.kt
+++ src/test/kotlin/days/Day3Test.kt
@@ -1,6 +1,7 @@
 package days
 
 import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.assertions.withClue
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
@@ -16,6 +17,48 @@ class Day3Test {
         assertSoftly(schematic) {
             withClue("width") { it.width shouldBe 10 }
             withClue("height") { it.height shouldBe 10 }
+        }
+    }
+
+    @Test
+    fun `Schematic can load a character from a point coordinates`() {
+        val data = InputReader("input_day_3.txt").getInputAsString()
+        val schematic = Day3.Schematic(data)
+
+        assertSoftly(schematic) {
+            getCharacterAt(x = 0, y = 0) shouldBe '4'
+            getCharacterAt(x = 0, y = 9) shouldBe '.'
+            getCharacterAt(x = 3, y = 1) shouldBe '*'
+            getCharacterAt(x = 0, y = 4) shouldBe '6'
+        }
+    }
+
+    @Test
+    fun `Trying to get character from an invalid point coordinate will throw an exception`() {
+        val data = InputReader("input_day_3.txt").getInputAsString()
+        val schematic = Day3.Schematic(data)
+
+        assertSoftly(schematic) {
+            withClue("Negative X") {
+                shouldThrow<IllegalArgumentException> {
+                    getCharacterAt(x = -1, y = 0)
+                }
+            }
+            withClue("Negative Y") {
+                shouldThrow<IllegalArgumentException> {
+                    getCharacterAt(x = 0, y = -1)
+                }
+            }
+            withClue("Too big X") {
+                shouldThrow<IllegalArgumentException> {
+                    getCharacterAt(x = 0, y = 10)
+                }
+            }
+            withClue("Too big Y") {
+                shouldThrow<IllegalArgumentException> {
+                    getCharacterAt(x = 10, y = 0)
+                }
+            }
         }
     }
 }
